cf3a442774dc54c15f51d8710d18cb74a0eba4fa
Mara Fabiana L. Benevides <marafabianalb@gmail.com>
2024-06-07 13:47:15 +0200

    Add comments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const checkAuth = require('../middlewares/checkAuth');
 
-// Register User /register
+// POST /register
 router.post('/register', userController.registerUser);
 
-// Login User /login
+// POST User /login
 router.post('/login', userController.loginUser);
 
 
